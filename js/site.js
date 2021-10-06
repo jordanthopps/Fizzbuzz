b54f4d2209d0904fa8e26178270a5c651f6972f9
jordanthopps <91483280+jordanthopps@users.noreply.github.com>
2021-10-05 20:04:34 -0400

    Fizzbuzz

--- js/site.js
+++ js/site.js
@@ -1,16 +1,70 @@
-function displayMessage() {
-    let msg = ""
+function getNumbers() {
+    let startValue = 0;
+    let endValue = 100;
 
-    msg = document.getElementById("message").value;
+    startValue = document.getElementById("startValue").value;
+    endValue = document.getElementById("endValue").value;
 
-    alert(msg);
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+
+        let numbers = generateNumbers(startValue, endValue);
+
+        displayNumbers(numbers)
+    } else {
+
+    }
+
 }
 
-function displayMessage2() {
-    let msg = "";
+function generateNumbers(startValue, endValue) {
+    let numbers = []
 
-    msg = document.getElementById("message").value;
+    for (let index = startValue; index <= endValue; index++) {
 
-    Swal.fire(msg);
-
+        numbers.push(index);
+    }
+    return numbers;
 }
+
+function displayNumbers(numbers) {
+
+    let startValue = numbers[0];
+    let endIndex = numbers.length;
+    let tableBody = document.getElementById("results")
+    tableBody.innerHTML = ""
+    for (let index = 0; index < endIndex; index++) {
+        let number = numbers[index];
+
+        //Order matters here. Start with the most stringent case.
+        if (number % 15 == 0) {
+            tableRow = `<tr><td>FizzBuzz</td></tr>`
+        } else if (number % 3 == 0) {
+            tableRow = `<tr><td>Fizz</td></tr>`
+        } else if (number % 5 == 0) {
+            tableRow = `<tr><td>Buzz</td></tr>`
+        } else {
+            tableRow = `<tr><td>${number}</td></tr>`
+        }
+
+        tableBody.innerHTML += tableRow;
+    }
+}
+
+/* function checkInputs(startValue, endValue) {
+
+    let errorMessage = "";
+
+    if (startValue < 1 || endValue < 1) {
+        errorMessage += "Please enter an integer between 1 and 100.";
+    }
+    if (startValue > 100 || endValue > 100) {
+        errorMessage += "Please enter an integer between 1 and 100.";
+    }
+    if (errorMessage.length > 0) {
+        alert("errorMessage");
+    }
+    break;
+}*/
